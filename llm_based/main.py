67620ec1efba8bf0aab13930f30c86f19f95ee66
Gabriel Ferreira <gabrielvictor.gferreira@gmail.com>
2025-04-05 00:13:29 -0500

    Organizing LLM Folder

--- llm_based/main.py
+++ llm_based/main.py
@@ -3,10 +3,10 @@ import os
 import json
 
 from langchain_ollama.llms import OllamaLLM
+from langchain.prompts import ChatPromptTemplate
 
 from prompts.template import TEMPLATE
-from utils.few_shot import create_few_shot_examples, few_shot_format
-from utils.inference import get_chain, run_single_prediction
+from prompts.few_shot import create_few_shot_examples, few_shot_format
 
 # Load the config file
 with open('../config/config.json', 'r') as f:
@@ -17,29 +17,34 @@ df_train = pd.read_csv(os.path.join(config["data_loc"], "train.csv"), encoding='
 df_test = pd.read_csv(os.path.join(config["data_loc"], "test.csv"), encoding='windows-1252')
 
 # Create few-shot examples
-few_shot_df = create_few_shot_examples(df_train, n=1)
+few_shot_df = create_few_shot_examples(df_train, n=2)
 prompt_context = "\n".join(few_shot_df.apply(few_shot_format, axis=1).tolist())
 
 # Initialize the model
 model = OllamaLLM(model="llama3.2")
-chain = get_chain(model, TEMPLATE)
+
+# Define the prompt template
+prompt = ChatPromptTemplate.from_template(TEMPLATE)
+
+# Create the chain
+chain = prompt | model 
 
 # Run one example
 row = df_test.iloc[1]
-result = run_single_prediction(
-    chain,
-    prompt_context,
-    row["Question"],
-    row["Response"],
-    row["CorrectAnswer"]
-)
+result = chain.invoke({
+        "prompt_context": prompt_context,
+        "question": row["Question"],
+        "student_response": row["Response"],
+        "correct_answer": row["CorrectAnswer"]
+        })
 
 # Output
-print("=== Prompt Sent ===\n", chain.prompt.format_messages(
+formatted_prompt = prompt.format_messages(
     prompt_context=prompt_context,
     question=row["Question"],
     student_response=row["Response"],
     correct_answer=row["CorrectAnswer"]
-)[0].content, "\n")
+)
+print("=== Prompt Sent ===\n", formatted_prompt[0].content, "\n")
 
 print("=== Result ===\n", result)
